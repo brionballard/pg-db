665bac46764c4f6af31f79bc5b6fa17c05dfc312
brionballard <brionballard@gmail.com>
2025-09-30 10:16:15 -0500

    fix: removed build from git

--- build/src/errors/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MissingRemoteSecret = exports.NotFoundError = exports.AppError = void 0;
-const AppError_1 = require("./AppError");
-Object.defineProperty(exports, "AppError", { enumerable: true, get: function () { return AppError_1.AppError; } });
-const NotFoundError_1 = require("./NotFoundError");
-Object.defineProperty(exports, "NotFoundError", { enumerable: true, get: function () { return NotFoundError_1.NotFoundError; } });
-const MissingRemoteSecret_1 = require("./MissingRemoteSecret");
-Object.defineProperty(exports, "MissingRemoteSecret", { enumerable: true, get: function () { return MissingRemoteSecret_1.MissingRemoteSecret; } });
